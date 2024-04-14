0c3aea116c2e1e76acca10ea304303b54f3915f7
Zakeria Hirsi <officialhirsi@gmail.com>
2024-04-14 23:27:17 +0100

    print macro properties, string interpolation, string formatting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, Zak!"); 
+
+    println!("{} days", 31);
+
+    println!("{:>5}", 1);
+
+
+    // #[allow(dead_code)] // disable `dead_code` which warn against unused module
+    // struct Structure(i32);
+
+    // println!("This struct `{}` won't print...", Structure(3));
+
+    //Format with precision
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi)
 }
